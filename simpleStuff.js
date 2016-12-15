b61b78a476d804390dfc9adf437762330ab11ff2
Don Nguyen <nguyen.donbui@gmail.com>
2016-12-15 18:06:58 -0500

    FileReader

--- simpleStuff.js
+++ simpleStuff.js
@@ -1,4 +1,13 @@
+var reader = new FileReader();
+
+reader.onload = function(e) {
+  var text = reader.result;
+  console.log(text);
+}
+
 function printFile(){
 	var selectedFile = $('#input').get(0).files[0];
-	console.log("selected file is: " + selectedFile);
+	console.log(selectedFile);
+
+	reader.readAsText(selectedFile);
 }
