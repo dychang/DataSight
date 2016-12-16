d56b9771e762064e84f6d4ce3a200efd5873c913
Don Nguyen <nguyen.donbui@gmail.com>
2016-12-16 03:19:47 -0500

    added chart types, 'the angular way', some styling

--- simpleStuff.js
+++ simpleStuff.js
@@ -1,13 +1,28 @@
-var reader = new FileReader();
+var app = angular.module('DataAggApp', []);
 
-reader.onload = function(e) {
-  var text = reader.result;
-  console.log(text);
-}
 
-function printFile(){
-	var selectedFile = $('#input').get(0).files[0];
-	console.log(selectedFile);
+app.controller('DataImportCtrl',[ '$scope', '$http', function($scope, $http) {
+	$scope.reader = new FileReader();
 
-	reader.readAsText(selectedFile);
-}
+	//Event Listeners
+	$scope.reader.onload = function(e) {
+	  $scope.fileText = $scope.reader.result;
+	  console.log($scope.fileText);
+	}
+
+	//Function Definitions
+	$scope.readFile = function() {
+		$scope.selectedFile = $('#input').get(0).files[0];
+		$scope.fileName = $scope.selectedFile.name;
+		console.log($scope.selectedFile);
+
+		$scope.reader.readAsText($scope.selectedFile);
+	}
+
+	//Handler for chart type selection buttons
+	$scope.chartTypeBtn = function(chartType) {
+		$scope.chartType = chartType;
+		$("#confirmModal").modal("toggle");
+	}
+	
+}]);
